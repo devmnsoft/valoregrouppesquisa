750cec7cc982a5478db7f983f753c4f80ba6e968
marcelo <marcelo.nascimento@cellent.com.br>
2026-08-20 14:22:45 -0300

    ajuste

--- backend/Valora.Web/Ui/ValoraIconRegistry.cs
+++ backend/Valora.Web/Ui/ValoraIconRegistry.cs
@@ -87,7 +87,7 @@ public sealed class ValoraIconRegistry
         ["file-up"] = "<path d=\"M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8Z M14 2v6h6M12 18v-6M9 15l3-3 3 3\"/>",
     };
 
-    public IReadOnlyCollection<string> KnownIcons => Icons.Keys;
+    public IReadOnlyCollection<string> KnownIcons => (IReadOnlyCollection<string>)Icons.Keys;
 
     public bool Contains(string? name) => !string.IsNullOrWhiteSpace(name) && Icons.ContainsKey(name);
 
